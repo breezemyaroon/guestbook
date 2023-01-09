fd095c0e141facf0cf4e311e0550147d17df48b5
breezemyaroon <breezemyaroon@gmail.com>
2023-01-09 12:08:17 +0700

    Gender

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <form action="show.php">
+        What is your name:<input type="text" name="name"><br>
+        What is your age:<input type="text" name="age"><br>
+        What gender are you:<input type="text" name="gender"><br>
+        <input type="submit">
+
+</body>
+
+</html>
---
+++ show.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+    $name = $_GET["name"];
+    $age = $_GET["age"];
+    $gender = $_GET["gender"];
+
+    echo "Name:".$name."<br>";
+    echo "Age:".$age."<br>";
+    echo "Gender:".$gender."<br>";
+    ?>
+
+</body>
+
+</html>
